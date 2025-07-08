86502727d8bd9467f683b2b23fe1ac3056321a42
Aline <alineoliveiradesantana2013@gmail.com>
2025-07-08 15:49:50 -0300

    Adicionado exercícios Stream API

---
+++ 03- Coleções e Stream em Java/10-Stream API/desafios/stream/api/Desafio03.java
@@ -0,0 +1,15 @@
+package desafios.stream.api;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio03 {
+
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        boolean numerosPositivos = numeros.stream()
+                .allMatch(n -> n > 0);
+
+        System.out.println(numerosPositivos);
+    }
+}
